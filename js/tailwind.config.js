61cfc178d7cc4f85aaf222af973b2b72ddf0379c
unknown <mohebbullah4045@gmail.com>
2023-06-20 03:28:51 +0600

    super fast

---
+++ js/tailwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        backgroundImage: {
+            'banner_img': "url('images/banner.png')",
+        
+          }
+        ,fontfamily:{
+          'poppins': ['Poppins', 'sans-serif'],
+
+        }
+        ,color:{
+          'white':['#fff']
+        }
+        ,boxShadow: {
+          'proparty_shadow': '-4px 4px 4px  rgba(0, 0, 0, 0.25)',
+        }
+      }
+    }
+  }
